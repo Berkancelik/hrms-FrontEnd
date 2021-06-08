87f0e84b4196e671ccf6b1926099695c7e9c5b8c
Berkancelik <celikberkan1@outlook.com>
2021-06-08 03:07:32 +0300

    Dashboard has updated

--- hrms-fe/layouts/Dashboard.jsx
+++ hrms-fe/layouts/Dashboard.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import Section from "./Section";
+import SideBar from "./SideBar";
 export default function Dashboard(){
     return(
         <div>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column width={4}>
-            <Categories />
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <ProductList />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={5}>
+              <SideBar/>
+            </Grid.Column>
+            <Grid.Column width={16}>
+                <Section/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+   
     </div>
-    )
+    );
 }
