483b7371ceef1037c1be945cc9e048d633d28391
Berkancelik <celikberkan1@outlook.com>
2021-06-16 16:54:33 +0300

    some has been updated

---
+++ hrms-fe/src/pages/JobAdvert.jsx
@@ -0,0 +1,55 @@
+import "./css/JobAdvert.css";
+
+import React from "react";
+export default function JobPost({ jobAdvertisement, setCurrentJobAdvert }) {
+  let defaultImage =
+    "https://res.cloudinary.com/diq6d5egr/image/upload/v1623606849/krpjh03bdcqshsprhrua.jpg";
+
+  function setJobDetails() {
+    setCurrentJobAdvert(jobAdvertisement);
+  }
+
+  function getHowLongAgo(publishedAt) {
+    let today = new Date();
+    let publishedDate = new Date(publishedAt);
+    let difference = today.getTime() - publishedDate.getTime();
+    let days = difference / (1000 * 3600 * 24);
+    if (days > 30) {
+      return Math.floor(days / 30) + " ay önce";
+    }
+    if (days > 7) {
+      return Math.floor(days / 7) + " hafta önce";
+    }
+    if (Math.floor(days) <= 0) {
+      return "Bugün";
+    }
+    return Math.floor(days) + " gün önce";
+  }
+  return (
+    <div>
+      <div class="job-advert">
+        <div class="company-image">
+          <img src={defaultImage} alt="" width="64" height="64" />
+        </div>
+        <div class="job-informations">
+          <div class="job-position">
+            <div onClick={() => setJobDetails()}>
+              {jobAdvertisement.jobTitle.name}
+            </div>
+          </div>
+          <div class="company-name">
+            <div>{jobAdvertisement.employer.companyName}</div>
+          </div>
+          <div class="city-name">
+            <span>{jobAdvertisement.city.name}</span>
+          </div>
+          <div>
+            <time datetime={jobPost.publishedAt} class="published-at">
+              {getHowLongAgo(jobPost.publishedAt)}
+            </time>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
