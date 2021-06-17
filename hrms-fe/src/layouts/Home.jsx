296d91e5d9374aefa93fab5e48ed2d104097acd5
Berkancelik <celikberkan1@outlook.com>
2021-06-18 01:09:36 +0300

    home has been updated

--- hrms-fe/src/layouts/Home.jsx
+++ hrms-fe/src/layouts/Home.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { Card,Grid} from 'semantic-ui-react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Button, Header, Icon } from "semantic-ui-react";
+
 export default function Home() {
-    return (
-        <div>
+  return (
+    <div className="image">
+      <Header inverted color="red" as="h1" icon textAlign="left">
+          
+        <Header.Content style={{ fontSize: "50px" }}>
+          HAYALİNDEKİ İŞİ BUL
 
-
-       
-        </div>
-    )
+        </Header.Content>
+      </Header>
+      <Header inverted color="red" as="h2" icon textAlign="left">
+        <Header.Content style={{ fontSize: "50px" }}>
+          Binlerce İş İlanı Arasında 
+          Kendine Uygun işi Hemen Bul!
+        </Header.Content>
+      </Header>
+      <Button as={NavLink} to="/jobAdvertisements" color="green" size="huge">
+        İŞ ARA
+        <Icon name="right arrow" />
+      </Button>
+    </div>
+  );
 }
