2eb4a64081230cf9f6861a3c09b72325fda765fc
Berkancelik <celikberkan1@outlook.com>
2021-06-24 15:07:19 +0300

    configureStore.js has been updated

---
+++ hrms-fe/src/store/configureStore.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
+export function configureStore() {
+    const middlewares = [thunk];
+    const composeEnhancers =
+        typeof window === 'object' &&
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+            window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            }) : compose;
+
+    const enhancer = composeEnhancers(
+        applyMiddleware(...middlewares),
+    );
+    return createStore(rootReducer, enhancer);
+    }
